2168d45139ce778e087a36e9dfadbe4a8fa3d3eb
TIAGO K <TIAGO K@TIAGOK>
2025-03-22 00:29:06 -0300

    feat: Adicionando testes de integração

---
+++ tests/CustomerApi.IntegrationTests/CustomerRepositoryTests/CustomerRepositoryCreate/CreateCustomerTests.cs
@@ -0,0 +1,18 @@
+﻿namespace CustomerApi.IntegrationTests.CustomerRepositoryTests.CustomerRepositoryCreate;
+public class CreateCustomerTests : CustomerRepositoryTestBase
+{
+  [Test]
+  public async Task CreateCustomer_Success()
+  {
+    await _customerRepository.Create(_fakeCustomer);
+
+    var customer = await _customerRepository.GetByEmail(_fakeCustomer.Email);
+
+    Assert.Multiple(() =>
+    {
+      Assert.That(_fakeCustomer.Name, Is.EqualTo(customer.Name));
+      Assert.That(_fakeCustomer.BirthDate, Is.EqualTo(customer.BirthDate));
+      Assert.That(_fakeCustomer.Email, Is.EqualTo(customer.Email));
+    });
+  }
+}
---
+++ tests/CustomerApi.IntegrationTests/CustomerRepositoryTests/CustomerRepositoryTestBase.cs
@@ -0,0 +1,49 @@
+﻿using Bogus;
+using CustomerApi.Domain.Customers;
+using CustomerApi.Infrastructure.Data;
+using CustomerApi.Infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CustomerApi.IntegrationTests.CustomerRepositoryTests;
+public abstract class CustomerRepositoryTestBase
+{
+  protected CustomerDbContext _dbContext;
+  protected CustomerRepository _customerRepository;
+  public Customer _fakeCustomer { get; set; }
+  protected Faker _faker;
+
+
+  public CustomerRepositoryTestBase()
+  {
+    var options = GetInMemoryContextOptions();
+
+    _dbContext = new CustomerDbContext(options);
+    _faker = new Faker("pt_BR");
+  }
+
+  [SetUp]
+  public void Setup()
+  {
+    _customerRepository = new CustomerRepository(_dbContext);
+
+    _fakeCustomer = new Customer(
+      _faker.Name.FullName(),
+      _faker.Date.Past(30, DateTime.Now.AddYears(-18)),
+      _faker.Internet.Email()
+    );
+  }
+
+  private static DbContextOptions<CustomerDbContext> GetInMemoryContextOptions()
+  {
+    var serviceProvider = new ServiceCollection()
+        .AddEntityFrameworkInMemoryDatabase()
+        .BuildServiceProvider();
+
+    var builder = new DbContextOptionsBuilder<CustomerDbContext>();
+    builder.UseInMemoryDatabase("mockdatabase")
+           .UseInternalServiceProvider(serviceProvider);
+
+    return builder.Options;
+  }
+}
