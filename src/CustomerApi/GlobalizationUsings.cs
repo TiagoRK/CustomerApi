c22dcf65aff543e0458b45462f932c8794089dbd
AgentBoard <agentboard@users.noreply.github.com>
2026-09-06 17:31:43 +0000

    AgentBoard 721f4bc4-86bf-4a0b-a7e2-b0d59b5599bf: cycle 2

---
+++ src/CustomerApi/GlobalizationUsings.cs
@@ -0,0 +1,8 @@
+using CustomerApi.Extensions;
+using CustomerApi.Web.Configurations;
+using CustomerApi.Web.Middlewares;
+using CustomerApi.Web.Filters;
+using CustomerApi.Infrastructure.IOC;
+using CustomerApi.Infraestructure.External.IOC;
+using Serilog;
+using Serilog.Extensions.Logging;
