3be2cdc1d998b83e9201d9895992152aa6dceda9
AgentBoard <agentboard@users.noreply.github.com>
2026-09-06 16:55:03 +0000

    AgentBoard 721f4bc4-86bf-4a0b-a7e2-b0d59b5599bf: cycle 1

--- src/CustomerApi/GlobalizationProgram.cs
+++ src/CustomerApi/GlobalizationProgram.cs
@@ -4,5 +4,53 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddGlobalization(builder.Configuration);
 
+var logger = Log.Logger = new LoggerConfiguration()
+  .Enrich.FromLogContext()
+  .WriteTo.Console()
+  .CreateLogger();
+
+logger.Information("Starting web host");
+
+builder.AddLoggerConfigs();
+
+var appLogger = new SerilogLoggerFactory(logger)
+    .CreateLogger<Program>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddLocalizationConfigs();
+builder.Services.AddSwaggerGen(options =>
+{
+  options.OperationFilter<IdempotencySwaggerOperationFilter>();
+});
+
+builder.Services.AddInfrastructureServices(builder.Configuration, appLogger);
+builder.Services.AddExternalInfrastructureServices(builder.Configuration, appLogger);
+builder.Services.AddServices(builder.Configuration, appLogger);
+
+builder.Services.AddHealthChecks();
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+app.UseLocalizationConfigs();
+
+app.UseAuthorization();
+
+app.UseExceptionHandler();
+app.UseMiddleware<IdempotencyMiddleware>();
+
+app.MapHealthChecks("/health-check");
+
+app.MapControllers();
+
+app.Run();
+
 // Existing registrations are preserved via the partial Program pattern;
 // this file hooks globalization in alongside the rest of startup.
--- src/CustomerApi/Program.cs
+++ src/CustomerApi/Program.cs
@@ -6,52 +6,6 @@ using CustomerApi.Web.Middlewares;
 using Serilog;
 using Serilog.Extensions.Logging;
 
-var logger = Log.Logger = new LoggerConfiguration()
-  .Enrich.FromLogContext()
-  .WriteTo.Console()
-  .CreateLogger();
 
-logger.Information("Starting web host");
-
-builder.AddLoggerConfigs();
-
-var appLogger = new SerilogLoggerFactory(logger)
-    .CreateLogger<Program>();
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddLocalizationConfigs();
-builder.Services.AddSwaggerGen(options =>
-{
-  options.OperationFilter<IdempotencySwaggerOperationFilter>();
-});
-
-builder.Services.AddInfrastructureServices(builder.Configuration, appLogger);
-builder.Services.AddExternalInfrastructureServices(builder.Configuration, appLogger);
-builder.Services.AddServices(builder.Configuration, appLogger);
-
-builder.Services.AddHealthChecks();
-
-builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-builder.Services.AddProblemDetails();
-
-var app = builder.Build();
-
-app.UseSwagger();
-app.UseSwaggerUI();
-
-app.UseHttpsRedirection();
-app.UseLocalizationConfigs();
-
-app.UseAuthorization();
-
-app.UseExceptionHandler();
-app.UseMiddleware<IdempotencyMiddleware>();
-
-app.MapHealthChecks("/health-check");
-
-app.MapControllers();
-
-app.Run();
 
 public partial class Program { }
