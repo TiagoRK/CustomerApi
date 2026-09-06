abe1f41d4d268f0181e672998ca032b27844362c
TIAGO K <TIAGO K@TIAGOK>
2026-09-06 14:50:33 -0300

    no message

--- src/CustomerApi/GlobalizationUsings.cs
+++
@@ -1,8 +0,0 @@
-using CustomerApi.Extensions;
-using CustomerApi.Web.Configurations;
-using CustomerApi.Web.Middlewares;
-using CustomerApi.Web.Filters;
-using CustomerApi.Infrastructure.IOC;
-using CustomerApi.Infraestructure.External.IOC;
-using Serilog;
-using Serilog.Extensions.Logging;
